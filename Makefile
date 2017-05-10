476b248ccc9c3ea1c954ce0aa8a5979c46dc4c70
beldpro <beldpro@gmail.com>
2017-05-10 15:19:39 -0300

    :rocket:

--- Makefile
+++ Makefile
@@ -1,18 +1,13 @@
 IMAGE_NAME	:= 	"loadbalancer"
 BRIDGE_GW 	:=	$(shell ./get-bridge-gateway.sh)
 
-
 image:
 	docker build \
 		--tag $(IMAGE_NAME) \
 		./image
 
-
 run:
-	docker run \
-		--detach \
-		--env CONTAINERS_ADDRESS=$(BRIDGE_GW) \
-		$(IMAGE_NAME)
+	CONTAINERS_ADDRESS=$(BRIDGE_GW) docker-compose up
 
 
 .PHONY: image run
--- loadbalancer/entrypoint.sh
+++ loadbalancer/entrypoint.sh
@@ -5,13 +5,14 @@ set -o errexit
 readonly RSYSLOG_PID="/var/run/rsyslogd.pid"
 
 main() {
+  ensure_environment_variable_set
+  template_haproxy_cfg
   start_rsyslog_in_background
   start_haproxy
 }
 
 ensure_environment_variable_set() {
-  test -z "$CONTAINERS_ADDRESS" \
-    && {
+  if [[ -z "$CONTAINERS_ADDRESS" ]]; then
       echo "Error:
     Environment variable 'CONTAINERS_ADDRESS' not set.
     Make sure you have initiated the container with the
@@ -20,7 +21,7 @@ ensure_environment_variable_set() {
 Aborting.
     "
       exit 1
-    }
+  fi
 }
 
 template_haproxy_cfg() {
